b9f4a1d3b4be2e77678623a4b5abe4592f026df2
delta <georgi.jolak@gmail.com>
2024-11-22 05:04:37 +0100

    Finished server properties

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,7 +1,14 @@
 package server;
 
+import server.network.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Server server = new Server();
+        } catch (Exception e) {
+            System.out.println("Failed to start server");
+            e.printStackTrace();
+        }
     }
 }
--- src/server/network/Server.java
+++ src/server/network/Server.java
@@ -1,19 +1,46 @@
 package server.network;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class Server {
-    private final int port = 8790;
+    private int port;
+    private String serverName;
+    private String[] bannedPhrases;
 
     private ServerSocket serverSocket;
 
     public Server() throws IOException {
+        loadProperties();
         serverSocket = new ServerSocket(port);
     }
 
     private void loadProperties() {
-        // TODO
+        Properties properties = new Properties();
+        String expectedFileLocation = "res/server.properties";
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(expectedFileLocation))) {
+            properties.load(reader);
+
+            port = Integer.parseInt(properties.getProperty("SERVER_PORT"));
+            serverName = properties.getProperty("SERVER_NAME");
+            bannedPhrases = Arrays.stream(properties.getProperty("BANNED_PHRASES").split(",")).map(String::trim).toArray(String[]::new);
+            System.out.println("Loaded properties");
+        } catch (IOException e) {
+            System.out.println("Could not find server.properties file");
+            System.out.println("Creating a template file");
+            port = 8790;
+            properties.setProperty("SERVER_PORT", port + "");
+            serverName = "Template";
+            properties.setProperty("SERVER_NAME", serverName);
+            bannedPhrases = new String[]{"nword", "javascript", "elon musk"};
+            properties.setProperty("BANNED_PHRASES", String.join(",", bannedPhrases));
+            try {
+                properties.store(new OutputStreamWriter(new FileOutputStream(expectedFileLocation)), null);
+            } catch (IOException e1) {
+                throw new RuntimeException("Could not create template file");
+            }
+        }
     }
 }
